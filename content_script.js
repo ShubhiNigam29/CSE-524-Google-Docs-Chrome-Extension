06e22a7b6be33a65dfc275a810d292d80bf15dc1
SHUBHI NIGAM <54998601+ShubhiNigam29@users.noreply.github.com>
2020-12-06 21:06:30 -0500

    Update content_script.js

--- content_script.js
+++ content_script.js
@@ -1,4 +1,4 @@
-let simulateMouseEvent = function(element, eventName, coordX, coordY) {
+simulateMouseEvent = function(element, eventName, coordX, coordY) {
   element.dispatchEvent(new MouseEvent(eventName, {
     view: window,
     bubbles: true,
@@ -9,11 +9,38 @@ let simulateMouseEvent = function(element, eventName, coordX, coordY) {
   }));
 };
 
-let buttonClick = document.querySelector("#printButton");
+// Repository to store mapping between Chrome Extension and Google Docs buttons
+map = {};
+map['undo'] = '#undoButton';
+map['redo'] = '#redoButton';
+map['print'] = '#printButton';
+map['spellingandgrammarcheck'] = '#spellGrammarCheckButton';
+map['paintformat'] = '#formatPainterButton';
+map['zoom'] = '#zoomSelect';
+map['styles'] = '#headingStyleSelect';
+map['font'] = '#docs-font-family';
+map['fontsize'] = '#fontSizeSelect';
+map['bold'] = '#boldButton';
+map['italics'] = '#italicButton';
+map['underline'] = '#underlineButton';
+map['textcolor'] = '#textColorButton';
+map['bgcolor'] = '#bgColorButton';
+map['insertlink'] = '#insertLinkButton';
+map['addcomment'] = '#insertCommentButton';
+map['insertimage'] = '#insertImageButton';
+map['align'] = '#alignButton';
+map['linespacing'] = '#lineSpacingMenuButton';
+map['numberedlist'] = '#addNumberedBulletButton';
+map['bulletedlist'] = '#addBulletButton';
+map['decreaseindent'] = '#outdentButton';
+map['increaseindent'] = '#indentButton';
+map['clearformatting'] = '#clearFormattingButton';
+
+buttonClick = document.querySelector(map[config]);
 
 // The getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
 // This method returns a DOMRect object with eight properties: left, top, right, bottom, x, y, width, height.
-let box = buttonClick.getBoundingClientRect(),
+box = buttonClick.getBoundingClientRect(),
     coordX = box.left + (box.right - box.left) / 2,
     coordY = box.top + (box.bottom - box.top) / 2;
 
