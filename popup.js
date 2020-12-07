f43b4628f417f049b88ab88c74e66b565ab659e3
SHUBHI NIGAM <54998601+ShubhiNigam29@users.noreply.github.com>
2020-12-06 21:05:13 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,7 +1,17 @@
-function injectScript() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.executeScript(tabs[0].id, {file: "content_script.js"});
-    });
-}
+let names = ['undo','redo','print','spellingandgrammarcheck','paintformat','zoom','styles','font','fontsize','bold','italics','underline','textcolor','bgcolor','insertlink','addcomment','insertimage','align','linespacing','numberedlist','bulletedlist','decreaseindent','increaseindent','clearformatting'];
 
-document.getElementById('print').addEventListener('click', injectScript);
+function Button(id) {
+  var element = document.getElementById(id);
+  element.onclick = function() {
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		chrome.tabs.executeScript(tabs[0].id, {
+		code: 'var config = ' + JSON.stringify(id)
+		}, function() {
+			chrome.tabs.executeScript(tabs[0].id, {file: 'content_script.js'});
+		});
+    });
+  }
+}
+for (var i = 0; i < names.length; i++) {
+	new Button(names[i]);
+}
